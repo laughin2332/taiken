cc6f99a71cd0411f66981157642bcc648df8e9bd
W-togara <99650625+W-togara@users.noreply.github.com>
2022-02-19 23:45:03 +0900

    Delete koukanndo directory

--- koukanndo/ARM/Debug/Package/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\81908\source\repos\koukanndo\koukanndo\ARM\Debug\Package\bin\AndroidManifest.xml : \
-C:\Users\81908\source\repos\koukanndo\koukanndo\ARM\Debug\Package\AndroidManifest.xml \
--- koukanndo/ARM/Debug/Package/gen/R.java.d
+++
@@ -1,3 +0,0 @@
-C:\Users\81908\source\repos\koukanndo\koukanndo\ARM\Debug\Package\gen\com\koukanndo\R.java \
- : C:\Users\81908\source\repos\koukanndo\koukanndo\ARM\Debug\Package\res\values\strings.xml \
-C:\Users\81908\source\repos\koukanndo\koukanndo\ARM\Debug\Package\bin\AndroidManifest.xml \
