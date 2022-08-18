35537340758fb45a8d16aa152ce813d514bab261
らーふぃん <99650625+laughin2332@users.noreply.github.com>
2022-08-18 13:44:54 +0900

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -3,13 +3,13 @@ import * as platform from "https://cdn.skypack.dev/platform@1.3.6";
 
 $(function(){
   let hour = 21600;
-  $('#ymd').text(new Date().toLocaleString());
-  $('#ip').text(platform.description);
   setInterval(() => {
     $('#s').text(Math.floor(hour / 3600));
     $('#e').text(Math.floor((hour % 3600) / 60));
     $('#c').text(Math.floor(hour % 60));
     hour = hour-1
   },1000)
+  $('#ymd').text(new Date().toLocaleString());
+  $('#ip').text(platform.description);
 })
   
